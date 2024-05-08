3094fddc467718e40b7bdd51b81a18db5d7d3c6e
funkylen <funkylen@gmail.com>
2024-05-08 16:03:29 +0300

    init

---
+++ images/app/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+
+
+class ImageLike(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.PROTECT)
+
+
+class Image(models.Model):
+    file = models.ImageField(upload_to="images")
+    created_at = models.DateTimeField(auto_created=True)
+    likes = models.ManyToManyField(ImageLike)
