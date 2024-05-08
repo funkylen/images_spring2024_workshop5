30a99df81580fbbb7213612d56f195af436c82f5
funkylen <funkylen@gmail.com>
2024-05-08 17:45:59 +0300

    add

--- images/app/views.py
+++ images/app/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
 
-from .forms import ImageForm
+from .forms import ImageForm, LoginForm
 
+
+@csrf_protect
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(request, **form.cleaned_data)
+            if user:
+                auth.login(request, user)
+                next_url = request.GET.get('next', '/')
+                return redirect(next_url)
+    else:
+        form = LoginForm
+
+    return render(request, 'login.html', {'form': form})
 
 # Create your views here.
 def index(request):
