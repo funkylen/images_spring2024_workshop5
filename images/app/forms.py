c5f4694b2b617a646c7a069dab633c9380a70f48
funkylen <funkylen@gmail.com>
2024-05-08 17:44:19 +0300

    forms

--- images/app/forms.py
+++ images/app/forms.py
@@ -3,7 +3,18 @@ from django import forms
 from .models import Image
 
 
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ['name', 'file']
+
+
+class EditImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['name']
