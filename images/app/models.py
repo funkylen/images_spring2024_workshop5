3165d3eb4928126716663afdb66661eaff17cbbc
funkylen <funkylen@gmail.com>
2024-05-08 16:31:19 +0300

    fix

--- images/app/models.py
+++ images/app/models.py
@@ -5,16 +5,16 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Image(models.Model):
     name = models.CharField(max_length=255)
     file = models.ImageField(upload_to="images")
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class ImageLike(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.PROTECT)
     image = models.ForeignKey(Image, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
